014c1dae5154524f599e27d712e7edcc781573fa
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2020-12-08 12:56:36 +0200

    Fix setup.py

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 # Download and setup HRNet
 HRNET=hrnet
 git clone https://github.com/LubomyrIvanitskiy/HRNet.git $HRNET
-sh $HRNET/setup.sh
+rm -r weights
+mkdir weights
+wget -nc -O weights/pose_hrnet_w32_256x192.pth https://www.dropbox.com/s/85toprbvlzzslyk/pose_hrnet_w32_256x192.pth?dl=0
